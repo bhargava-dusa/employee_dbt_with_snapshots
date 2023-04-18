79b9778bb8e18736eb0b8c010fa35a21d2c02365
Bhargava Chandra Dusa <bhargava.dusa@zemosolabs.com>
2023-04-18 23:03:29 +0530

    minor fix

--- snapshots/employee_snapshot.sql
+++ snapshots/employee_snapshot.sql
@@ -5,12 +5,11 @@
       target_database='mydb',
       target_schema='snapshots',
       unique_key='employee_id',
-
       strategy='timestamp',
       updated_at='updated_at',
     )
 }}
 
-select * from {{ ref('raw_data', 'employee') }}
+select * from {{ ref('employee') }}
 
 {% endsnapshot %}
