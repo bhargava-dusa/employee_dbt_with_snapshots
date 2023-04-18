7571879aa595bc8418b66b151b25352fc929588d
Bhargava Chandra Dusa <bhargava.dusa@zemosolabs.com>
2023-04-18 15:58:53 +0530

    snapshots

---
+++ snapshots/employee_snapshot.sql
@@ -0,0 +1,16 @@
+{% snapshot employee_snapshot %}
+
+{{
+    config(
+      target_database='mydb',
+      target_schema='snapshots',
+      unique_key='employee_id',
+
+      strategy='timestamp',
+      updated_at='updated_at',
+    )
+}}
+
+select * from {{ source('raw_data', 'employee') }}
+
+{% endsnapshot %}
